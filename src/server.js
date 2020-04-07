a38f8db146838fb124790f7c58d5ebec7f93f445
kshitij zutshi <kshitij.zutshi@gmail.com>
2020-04-07 15:56:13 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ NOTE
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+const publicPath = path.join(__dirname, "client/build");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
