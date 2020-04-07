499eeb7cd5fb6c9856f40309ebbcbbea1ed23ded
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-04-07 14:29:24 +0530

    'comment'

---
+++ src/server.js
@@ -0,0 +1,20 @@
+/*
+NOTE
+====
+ // While deploying on heroku change "start": "react-scripts start" to "start": "node server/server.js" IN PACKAGE.JSON FILE
+
+
+*/
+
+const path = require("path");
+const express = require("express");
+const app = express();
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+app.listen(port, () => {
+  console.log("Server is up!");
+});
